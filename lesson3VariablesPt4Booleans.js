80bf560e4c005ea9445eec675c8b91f92ad4b2ea
Ryan <ryan.e.tibbetts@gmail.com>
2020-08-14 14:59:05 -0400

    added boolean lesson

---
+++ lesson3VariablesPt4Booleans.js
@@ -0,0 +1,15 @@
+let turth, falsh, undethiccd;
+turth = true;
+falsh = false;
+undethiccd = undefined;
+
+console.log('this is ', turth);
+console.log('this is ', falsh);
+console.log('this is ', undethiccd);
+
+console.log('this is ', turth + turth);
+console.log('this is ', turth + falsh);
+console.log('this is ', falsh + falsh);
+console.log('this is ', turth + undethiccd);
+console.log('this is ', falsh + undethiccd);
+console.log('this is ', undethiccd + undethiccd);
